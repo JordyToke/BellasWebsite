afa16173f753f6edededfed9fc21b755ac3a991f
JordyToke <jordyn.toke@gmail.com>
2025-04-23 17:02:27 +1000

    fixed vite config base from '/' to './'

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.css";
 const Home = () => (
   <section className="home">
     <div className={styles.slideshow}>
-      <img className={styles.heroImage} src="IMG_7983.jpeg" alt="" />
+      <img className={`${styles.heroImage}`} src="IMG_7983.jpeg" alt="" />
     <p>
       The above will be a Gallery slideshow.
     </p>
